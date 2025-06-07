bffe4b6fd20995c0ef12ee0d388047b24f67669f
Mayra Alejandra <mveliz4134@utm.edu.ec>
2025-06-07 14:36:11 -0500

    se agrega ejercio de calculadora

---
+++ Calculadora-main/script.js
@@ -0,0 +1,17 @@
+let pantalla = document.getElementById('pantalla');
+
+function agregar(valor) {
+  pantalla.value += valor;
+}
+
+function limpiar() {
+  pantalla.value = '';
+}
+
+function calcular() {
+  try {
+    pantalla.value = eval(pantalla.value);
+  } catch {
+    pantalla.value = 'Error';
+  }
+}
